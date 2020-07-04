f6a57a23d1b38454b8f6de38b4fe3f2dd3966a47
Douglas Hirsh <dhirshjr@gmail.com>
2020-07-04 18:40:09 -0500

    feat: Add layout template.

--- src/main/java/today/whereismystuff/web/controllers/HomeController.java
+++ src/main/java/today/whereismystuff/web/controllers/HomeController.java
@@ -2,13 +2,11 @@ package today.whereismystuff.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
     public String home() {
-        return "I'm working!";
+        return "home";
     }
 }

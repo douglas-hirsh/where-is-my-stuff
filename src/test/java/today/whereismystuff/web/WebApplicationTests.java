a5aa3a5a22268af631efdfd8002907027940b38c
Douglas Hirsh <dhirshjr@gmail.com>
2020-07-07 09:39:13 -0500

    Add initial integration testing.

--- src/test/java/today/whereismystuff/web/WebApplicationTests.java
+++ src/test/java/today/whereismystuff/web/WebApplicationTests.java
@@ -1,13 +1,42 @@
 package today.whereismystuff.web;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class WebApplicationTests {
-
+    @Autowired
+    private MockMvc mvc;
     @Test
     void contextLoads() {
+        assertNotNull(mvc);
     }
+
+    @Test
+    void canGetItemsIndex() throws Exception {
+        mvc.perform(get("/items"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Items")));
+    }
+
+
 
 }
